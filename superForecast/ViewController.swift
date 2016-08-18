9bb60590957e6381cc761dfa9a423d768cc80dc8
nosupe4u <Nosupe4u@lovells-macbook.local>
2016-08-17 19:24:51 -0700

    Initial Commit

---
+++ superForecast/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  superForecast
+//
+//  Created by Nosupe4u on 8/17/16.
+//  Copyright © 2016 Nosupe4u. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
