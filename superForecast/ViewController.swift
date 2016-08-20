915b152aac064a8c7ee4e782c9c19946bd8b9669
nosupe4u <Nosupe4u@Lovells-MacBook.local>
2016-08-20 02:13:36 -0700

    UI Picked added

--- superForecast/ViewController.swift
+++ superForecast/ViewController.swift
@@ -8,20 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {//Protocols:
 
+    @IBOutlet weak var statePickerBtn: UIButton!
+    @IBOutlet weak var statePicker: UIPickerView!
+    
+    let states = ["Alabama","LSU","Miami","USC"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        statePicker.dataSource = self
+        statePicker.delegate = self //Come talk to the ViewController
     }
 
-    var hello = "Hello Brooklyn"
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func stateBtnPressed(_ sender: AnyObject) {
+        statePicker.isHidden = false
+    }
 
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statePickerBtn.setTitle(states[row], for: UIControlState.normal)
+        statePicker.isHidden = true
+    }
 }
 
